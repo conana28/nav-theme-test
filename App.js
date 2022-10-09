904eec6160961feca8fb6b8dc66df96f9b33e631
conana28 <33334673+conana28@users.noreply.github.com>
2022-10-09 22:51:51 +1300

    Add Navigation Darke Theme

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import * as React from "react";
 import { Button, View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+} from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useTheme } from "@react-navigation/native";
 
 function HomeScreen({ navigation }) {
   return (
@@ -24,11 +29,24 @@ function HomeScreen({ navigation }) {
 function DetailsScreen({ route, navigation }) {
   /* 2. Get the param */
   const { itemId, otherParam } = route.params;
+  const { colors } = useTheme();
+  console.log(colors);
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ color: colors.text }}>Details Screen</Text>
+      <Text style={{ color: colors.primary }}>
+        itemId: {JSON.stringify(itemId)}
+      </Text>
+      <Text style={{ color: colors.text }}>
+        otherParam: {JSON.stringify(otherParam)}
+      </Text>
       <Button
         title="Go to Details... again"
         onPress={() =>
@@ -47,7 +65,7 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={DarkTheme}>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
