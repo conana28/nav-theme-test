d83281a76cc711ddb0b298e15f349de8941d80ff
conana28 <33334673+conana28@users.noreply.github.com>
2022-10-18 22:00:38 +1300

    First Edit Dialog Commit

---
+++ src/screens/Dialogs/CellarEditDialog.js
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+import React from "react";
+
+const CellarEditDialog = () => {
+  return (
+    <View>
+      <Text>CellarEditDialog!!</Text>
+    </View>
+  );
+};
+
+export default CellarEditDialog;
